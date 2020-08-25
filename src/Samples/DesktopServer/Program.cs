ea4fdacc7a421de9111026cb08f0a8a4c4a9b0bb
Daniel Crenna <daniel.crenna@gmail.com>
2020-08-25 16:19:58 -0400

    turn on WSDL generation in the Desktop sample

--- src/Samples/DesktopServer/Program.cs
+++ src/Samples/DesktopServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,14 @@ namespace DesktopServer
                 new Uri("http://localhost:8080/"));
             host.AddServiceEndpoint(typeof(Contract.IEchoService), new NetTcpBinding(), "/nettcp");
             host.AddServiceEndpoint(typeof(Contract.IEchoService), new BasicHttpBinding(), "/basichttp");
+
+            var meta = new ServiceMetadataBehavior();
+            meta.HttpGetEnabled = true;
+            meta.HttpGetUrl = new Uri("http://localhost:8080/basichttp");
+            host.Description.Behaviors.Add(meta);
+
             host.Open();
+
             foreach(var endpoint in host.Description.Endpoints)
             {
                 Console.WriteLine("Listening on " + endpoint.ListenUri.ToString());
